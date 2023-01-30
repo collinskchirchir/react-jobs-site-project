49f0010b85814ee0c13b986b85661260005eea6a
Collins Chirchir <collinskchirchir@gmail.com>
2023-01-30 14:32:08 +0300

    Header-content added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import logo from '../img/logo1.svg';
 import hero from '../img/hero.jpg';
+import search from '../img/search.svg'
 /*eslint-disable*/
 function Header() {
   return (
@@ -9,7 +10,7 @@ function Header() {
       <nav className='navigation'>
         <div className='nav-wrapper'>
           <div className='logo'>
-            <img src={logo}   alt=''></img>
+            <img src={logo}   alt='Logo '></img>
           </div>
           <ul>
             <li>
@@ -31,6 +32,35 @@ function Header() {
           <a href='#' className='cta-btn'>+ Create Account</a>
         </div>
       </nav>
+      {/* Header Content */}
+      <div className='header-content'>
+        <h1>
+            Over <span>10 000</span> jobs available <br />
+            You can choose your dream job
+        </h1>
+        <p className='main-para'>
+          Find a great job for building your bright career. We have many openings available
+        </p>
+        <div className='input-control'>
+          <input type="text" placeholder='Job Title of Keywords' />
+          <select name='location' id='location'>
+            <option value=''>London</option>
+            <option value=''>Manchester</option>
+            <option value=''>Liverpool</option>
+            <option value=''>Swansea</option>
+          </select>
+          <select name='jobs' id='jobs'>
+            <option value=''>Game Development</option>
+            <option value=''>Game Desinging</option>
+            <option value=''>Web Design</option>
+            <option value=''>We Development</option>
+            <option value=''>UI / UX</option>
+          </select>
+          <button className='search-btn'>
+            <img src={search} alt='Button' /> 
+          </button>
+        </div> 
+      </div>
     </HeaderStyled>
   )
 }
@@ -56,7 +86,7 @@ const HeaderStyled = styled.header`
       display: flex;
       align-items: center;
       li{
-        margin: 0 1rem
+        margin: 0 1rem;
       }
     }
     .cta-btn{
